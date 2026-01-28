d9f113109956d6cd25ba3007bc5b8cd669634c3c
Ann-Sophie Allgaier <ann-sophie.allgaier@tum.de>
2026-01-28 21:39:33 +0100

    Data done

--- project.R
+++ project.R
@@ -1,28 +1,36 @@
 library(readr)
 library(tidyverse)
 library(ggplot2)
+library(patchwork)
+
 
 #importing data 
-aapl_master_enriched <- read_csv("archive (2)/aapl_master_enriched.csv")
-sap500 <- read_csv("archive (3)/sap500.csv")
-view(aapl_master_enriched)
-view(sap500)
+aapl_master_enriched <- read_csv("Data/archive (2)/aapl_master_enriched.csv")
+sap500 <- read_csv("Data/archive (3)/sap500.csv")
+X_NASDAQ <- read_csv("Data/_NASDAQ.csv")
 
 #Reducing data to sensible amount, Align data by date
-aapl_master_enriched <- filter(aapl_master_enriched, date >= as.Date("2020-01-01") & date <= as.Date("2026-01-23"
+aapl_master_enriched <- filter(aapl_master_enriched, date >= as.Date("2020-01-01") & date <= as.Date("2025-11-26"
 ))
-aapl_master_enriched2 <- filter(aapl_master_enriched, date >= as.Date("2026-01-01") & date <= as.Date("2026-01-23"
+aapl_master_enriched <- mutate(aapl_master_enriched, log_volume = log(volume))
+sap500 <- filter(sap500, Date >= as.Date("2020-01-01") & Date <= as.Date("2025-11-26"
 ))
-sap500 <- filter(sap500, Date >= as.Date("2020-01-01") & Date <= as.Date("2026-01-23"
+nasdaq <- filter(X_NASDAQ, Date >= as.Date("2020-01-01") & Date <= as.Date("2025-11-26"
 ))
-table <- tibble(date = aapl_master_enriched$date, aapl_close = aapl_master_enriched$close, aapl_open = aapl_master_enriched$open, aapl_volume =  aapl_master_enriched$volume, sp500_open = sap500$Open, sp500_volume = sap500$Volume)
+
+table <- tibble(date = aapl_master_enriched$date, aapl_close = aapl_master_enriched$close, aapl_open = aapl_master_enriched$open, 
+                aapl_volume =  aapl_master_enriched$log_volume, sp500_open = sap500$Open, nasdaq_open = nasdaq$Open,
+                aapl_ema = aapl_master_enriched$ema_5)
 view(table)
 
 #Vorraussetzungen 
 
 #Linearität 
- ggplot(aapl_master_enriched, aes(x = return_1d, y = close))+geom_point()
- ggplot(table, aes(x = aapl_open, y =  aapl_close))+geom_point()
- ggplot(table, aes(x= log(aapl_volume), y = aapl_close))+geom_point()
- ggplot(table, aes(x= sp500_open, y = aapl_close))+geom_point() 
- ggplot(table, aes(x= log(sp500_volume), y = aapl_close))+geom_point() 
+ p1 <- ggplot(table, aes(x = aapl_open, y =  aapl_close))+geom_point()
+ p2 <- ggplot(table, aes(x= (aapl_volume), y = aapl_close))+geom_point()
+ p3 <- ggplot(table, aes(x= sp500_open, y = aapl_close))+geom_point() 
+ p4 <- ggplot(table, aes(x= nasdaq_open, y = aapl_close))+geom_point()
+ p5 <- ggplot(table, aes(x = aapl_ema, y = aapl_close)) + geom_point()
+ (combined <-  p1 + p2 + p3 + p4 + p5)
+ 
+ 
