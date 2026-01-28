5dde2e2480339175a5c5df3afaed663928117877
Ann-Sophie Allgaier <ann-sophie.allgaier@tum.de>
2026-01-28 15:51:22 +0100

    Data

--- project.R
+++ project.R
@@ -1,4 +1,15 @@
-(x <- 10)
+library(readr)
+aapl_master_enriched <- read_csv("archive (2)/aapl_master_enriched.csv")
+View(aapl_master_enriched)
+aapl_quarterly_master <- read_csv("archive (2)/aapl_quarterly_master.csv")
+View(aapl_quarterly_master)
+aapl_quarterly_summary <- read_csv("archive (2)/aapl_quarterly_summary.csv")
+View(aapl_quarterly_summary)
+aapl_stock_ml_features <- read_csv("archive (2)/aapl_stock_ml_features.csv")
+View(aapl_stock_ml_features)
 
+
+class(aapl_master_enriched)
+aapl_master_enriched
 
 
